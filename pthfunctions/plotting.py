a1b989a0143b64bb1b0012e0adb906dfc14b1f8a
Yeray Martinez <yeraym12@gmail.com>
2024-04-15 22:53:36 +0200

    plot_results_comparison customization
    Added customization for plotting a single metric and sizing

--- pthfunctions/plotting.py
+++ pthfunctions/plotting.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from typing import Tuple
 
 
 def _convert_metric_name(metric_name: str) -> str:
@@ -17,26 +18,41 @@ def _convert_metric_name(metric_name: str) -> str:
   return ' '.join(words)
 
 
-def plot_results_comparison(**kwargs: pd.DataFrame):
+def plot_results_comparison(metric_to_plot: str = None, size: Tuple[float, float] = (15, 10), **kwargs: pd.DataFrame) -> None:
   """
   Plots a comparison of all models' results, including train/test loss and accuracy,
   given that they trained for the same number of epochs.
 
   Args:
+      metric_to_plot: A string representing the metric to plot ("train_loss", "test_loss",
+        "train_acc", and "test_acc"). If None, all metrics are plotted.
+      size: A tuple of floats representing the size of the plot.
       **kwargs: Named arguments where keys are model names and values are pandas DataFrames
-                 containing training and testing data. Each DataFrame is expected to have
-                 columns named "train_loss", "test_loss", "train_acc", and "test_acc".
+        containing training and testing data. Each DataFrame is expected to have columns
+        named "train_loss", "test_loss", "train_acc", and "test_acc".
+  
+  Returns:
+      None if the plot is successfully displayed.
+      ValueError if the metric_to_plot is not a valid metric name.
   """
-  plt.figure(figsize=(15, 10))
-  metrics = ["train_loss", "test_loss", "train_acc", "test_acc"]
+  valid_metrics = ["train_loss", "test_loss", "train_acc", "test_acc"]
 
-  for i, metric in enumerate(metrics):
-    plt.subplot(2, 2, i+1)
+  if metric_to_plot:
+    if metric_to_plot not in valid_metrics:
+      raise ValueError(f"Invalid metric: {metric_to_plot}. Valid options are: {', '.join(valid_metrics)}")
+    metrics = [metric_to_plot]
+  else:
+    metrics = valid_metrics
 
+  plt.figure(figsize=size)
+
+  for metric in metrics:
     for model_name, df in kwargs.items():
       epochs = range(len(df))
       plt.plot(epochs, df[metric], label=model_name)
-      
+
     plt.title(_convert_metric_name(metric))
     plt.xlabel("Epochs")
     plt.legend()
+
+  plt.show()
